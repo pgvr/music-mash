dafe046549cc70c6f790e08ed448b3e656827dc3
Patrick Göler von Ravensburg <mail@pgvr.dev>
2019-12-28 21:16:24 +0100

    add update message for service worker

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { Router } from "@angular/router"
-import { NbThemeService } from "@nebular/theme"
+import { NbThemeService, NbToastrService } from "@nebular/theme"
+import { SwUpdate } from "@angular/service-worker"
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  constructor(public router: Router, private themeService: NbThemeService) {}
+export class AppComponent implements OnInit {
+  constructor(
+    public router: Router,
+    private themeService: NbThemeService,
+    private swUpdate: SwUpdate,
+    private toastrService: NbToastrService,
+  ) {}
+
+  ngOnInit() {
+    this.swUpdate.available.subscribe(evt => {
+      // an update is available, add some logic here.
+      this.toastrService.info("Reload to update the app", "Update Available", {
+        icon: "gift-outline",
+        duration: 5000,
+      })
+    })
+  }
 
   startNewParty() {
     this.router.navigateByUrl("/party")
